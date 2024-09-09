ce3dc64a62f66072caf68470cec5e81c5b62436c
alexaalvaran <130896929+alexaalvaran@users.noreply.github.com>
2024-09-09 13:45:14 +1200

    set up for testing

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('HI!');
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 
--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'HI!';
+    return this.appService.getHello();;
   }
 }
