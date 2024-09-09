600f78af923f02d4e5dbe9ea4cf4578a40852e5b
alexaalvaran <130896929+alexaalvaran@users.noreply.github.com>
2024-09-09 13:46:44 +1200

    set up for testing

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -19,11 +19,4 @@ describe('AppController', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
-
-  describe('Addition', () => {
-    it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-    });
-    });
-    
 });
