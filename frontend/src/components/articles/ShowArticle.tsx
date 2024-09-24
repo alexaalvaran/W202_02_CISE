39662d30400e431d1833e6691c33c9cae9f30950
Albert <krm9346@autuni.ac.nz>
2024-09-25 05:23:28 +0700

    changes on search

--- frontend/src/components/articles/ShowArticle.tsx
+++ frontend/src/components/articles/ShowArticle.tsx
@@ -9,32 +9,13 @@ export default function ShowArticle() {
     const { id } = useParams(); // Get the ID from the route
     const [article, setArticle] = useState<Article | null>(null);
     const router = useRouter(); // To navigate between pages
-    
+
     useEffect(() => {
-        // Ensure that the component is mounted and id exists
         if (id) {
             const foundArticle = mockResults.find((article) => article._id === id);
             setArticle(foundArticle || null);
         }
     }, [id]);
-
-
-    /* useEffect(() => {
-        const fetchArticle = async () => {
-            try {
-                const response = await fetch(`/api/articles/${id}`);
-                const data = await response.json();
-                setArticle(data);
-            } catch (error) {
-                console.error('Error fetching article:', error);
-            }
-        };
-    
-        if (id) {
-            fetchArticle();
-        }
-    }, [id]); */
-    
 
     if (!article) {
         return <p>Loading article...</p>;
@@ -42,7 +23,7 @@ export default function ShowArticle() {
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
-            <div className="card p-12" style={{ maxWidth: '600px', width: '100%' }}>
+            <div className="card p-4" style={{ maxWidth: '600px', width: '100%' }}>
                 <h1><strong>{article.title}</strong></h1>
                 <p><strong>Author:</strong> {article.authors}</p>
                 <p><strong>Publication Year:</strong> {article.pubyear}</p>
