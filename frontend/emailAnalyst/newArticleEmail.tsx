8b8c921118e9d2a295a49590be2998e3675ef78a
AdrienneChiu <131317952+AdrienneChiu@users.noreply.github.com>
2024-10-01 13:00:20 +1300

    made changes to email file

--- frontend/emailAnalyst/newArticleEmail.tsx
+++ frontend/emailAnalyst/newArticleEmail.tsx
@@ -1,37 +1,41 @@
-import React from "react";
-import { Article } from "@/components/articles/Article";
-import { Body, 
-    Container,
-    Button,
-    Head, 
-    Heading, 
-    Html, 
-    Preview, 
-    Tailwind,
-    Text } 
-    from "@react-email/components";
+import { Button } from "@react-email/components";
 
-export default function newArticle() {
-    return(
-        <Html>
-            <Head />
-            <Preview>New update to SPEED app</Preview>
-            <Tailwind>
-                <Body className='bg-white text-black font-sans'>
-                    <Container className='mt-8 border border-black p-10 mx-auto'>
-                        <Heading>You got new articles to review!</Heading>
-                       <Text> 
-                        You have new articles waiting to be reviewed. Please login and check.
-                       </Text>
-                       <Button
-                        href="/login"
-                        className='big-blue-700 text-black px=4 py-2'
-                        >
-                        Go to App
-                       </Button>
-                    </Container>
-                </Body>
-            </Tailwind>
-        </Html>
-    )
-}
+const nodemailer = require("nodemailer");
+//require("dotenv").config();
+
+//Creates resusable transporter object
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: "rienne0203@gmail.com", //email sent from this account
+      pass: "tlck slmc gwhs ucgv", //gmail generated password
+    },
+  });
+
+  const mailOptions = {
+    from: {
+        name: 'Adrienne Chiu',
+        address:'rienne0203@gmail.com'
+    }, //sender email
+    to: "achiu.heart@gmail.com, ymw7320@autuni.ac.nz",  //reciever email
+    subject: "APP new articles", //email subject line 
+    text: "New articles waiting to be reviewed", //email body text
+    html: "<b>You have new articles waiting to be reviewed. Please login and check.</b>", //html body text
+    
+  }
+
+
+
+    //function to find errors from transporter
+     transporter.sendMail(mailOptions, function(error: string, info: { response: string; }){
+    if (error) {
+      console.log("Error:" + error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  
